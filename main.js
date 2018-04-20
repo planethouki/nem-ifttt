5970d793f190a2fb9ee01d6124873f874752ab0e
planethouki <syn_tok@hotmail.co.jp>
2018-04-20 10:42:05 +0900

    https

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-var http = require('http');
-http.createServe(function(request, response) {
+var http = require('https');
+https.createServe(function(request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
   response.end('Hello World\n');
 }).listen(process.env.PORT || 8080);
