74c4fef948df34ec77854bdf1bf1b2da63415694
planethouki <syn_tok@hotmail.co.jp>
2018-04-24 01:37:52 +0000

    websock?

--- Procfile
+++ Procfile
@@ -1,1 +1,3 @@
-web: node requests-temp1.js
+#web: node requests-temp1.js
+#web: node index.js
+web: node websockets-temp1.js
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var http = require('http');
 http.createServer(function(request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.write(process.env.IFTTT_KEY+'\n')
   response.end('Hello World\n');
 }).listen(process.env.PORT || 8080);
