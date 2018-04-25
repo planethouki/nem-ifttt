a22929d447ca1a45bf2f91ce3f340657c2c126a4
planethouki <syn_tok@hotmail.co.jp>
2018-04-25 02:46:11 +0000

    procfile

--- Procfile
+++ Procfile
@@ -1,5 +1,4 @@
-web: node monitor-temp3.js
-main: node index.js
+web: node index.js
 req: node requests-temp1.js
 soc: node websockets-temp1.js
 mon: node monitor-temp3.js
--- monitor-temp3.js
+++ monitor-temp3.js
@@ -77,9 +77,3 @@ function postifttt(value) {
     //console.log(body);
   });
 }
-
-var http = require('http');
-http.createServer(function(request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello World\n');
-}).listen(process.env.PORT || 8080);
